185849dea83fc12cd17b45042996ff755a47fde8
Musab-Dev <musab.dev98@gmail.com>
2024-01-21 13:29:26 +0300

    [database] Job model, factory, seeder implemented

---
+++ database/factories/JobFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Job;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
+ */
+class JobFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->jobTitle,
+            'description' => fake()->paragraphs(3, true),
+            'salary' => fake()->numberBetween(5_000, 100_000),
+            'experience' => fake()->randomElement(Job::$experiences),
+            'category' => fake()->randomElement(Job::$categories),
+        ];
+    }
+}
