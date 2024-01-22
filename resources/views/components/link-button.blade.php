0858fe830774c00212921003f9040e336bd01106
Musab-Dev <musab.dev98@gmail.com>
2024-01-22 11:42:22 +0300

    [feature] job details view

---
+++ resources/views/components/link-button.blade.php
@@ -0,0 +1,4 @@
+<a href="{{ $href }}"
+    class="border border-slate-200 rounded-md font-semibold text-black px-4 py-2 hover:bg-slate-200">
+    {{ $slot }}
+</a>
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return redirect()->route('jobs.index');
-});
+Route::get('/', fn () => to_route('jobs.index'));
 
 Route::resource('jobs', JobController::class);
